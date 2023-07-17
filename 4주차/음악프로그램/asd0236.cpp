22d795f2b8c436fceeb44592e51b3aa4720e4dfa
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-07-17 21:07:42 +0900

    Docs : 폴더 경로 수정

--- 4주차/음악프로그램/asd0236.cpp
+++ 4주차/음악프로그램/asd0236.cpp
@@ -8,14 +8,18 @@ int n, m, k, num, degree[1001];
 vector<int> ans, graph[1001];
 queue<int> q;
 
-void input() {
+void input()
+{
 	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < m; i++)
+	{
 		cin >> k;
-		for (int j = 0; j < k; j++) {
+		for (int j = 0; j < k; j++)
+		{
 			int prev = num;
 			cin >> num;
-			if (j) {
+			if (j)
+			{
 				degree[num]++;
 				graph[prev].push_back(num);
 			}
@@ -23,33 +27,44 @@ void input() {
 	}
 }
 
-void solve() {
+void solve()
+{
 
-	for (int i = 1; i <= n; i++) {
-		if (degree[i] == 0) q.push(i);
+	for (int i = 1; i <= n; i++)
+	{
+		if (degree[i] == 0)
+			q.push(i);
 	}
 
-	while (!q.empty()) {
+	while (!q.empty())
+	{
 		int cur = q.front();
 		q.pop();
-		
+
 		ans.push_back(cur);
 
-		for (auto i : graph[cur]) {
-			if (--degree[i] == 0) q.push(i);
+		for (auto i : graph[cur])
+		{
+			if (--degree[i] == 0)
+				q.push(i);
 		}
 	}
 
-	for (int i = 1; i <= n; i++) {
-		if (degree[i]) {
-			cout << '0'; return;
+	for (int i = 1; i <= n; i++)
+	{
+		if (degree[i])
+		{
+			cout << '0';
+			return;
 		}
 	}
 
-	for (auto i : ans) cout << i << '\n';
+	for (auto i : ans)
+		cout << i << '\n';
 }
 
-int main() {
+int main()
+{
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
 
