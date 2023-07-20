a32d05a57fb6699ede703e35a86c7a5fb43eba45
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-20 16:23:11 +0900

    Feat: 4주차 수찾기

---
+++ 4주차/수 찾기/sionel.js
@@ -0,0 +1,13 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const [a, b, c, d] = input.map((v) => v.split(" "));
+const set = new Set(b);
+let result = "";
+d.forEach((e) => {
+  result += set.has(e) ? "1" : "0";
+  result += "\n";
+});
+console.log(result);
