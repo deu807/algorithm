4ffdb6c9c6fd51fbf0a1a6f29a3d74337bf80efe
seongheonjeong <js504040@naver.com>
2023-08-26 23:11:24 +0900

    Feat:정성헌 9주차 오큰수 문제풀이

---
+++ 9주차/오큰수/정성헌.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int N, arr[1000001], ans[1000001], num;
+
+int main()
+{
+	stack<int> s;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		arr[i] = num;
+
+		while (!s.empty() && arr[s.top()] < num)
+		{
+			ans[s.top()] = num;
+			s.pop();
+		}
+
+		s.push(i);
+	}
+
+	while (!s.empty())
+	{
+		ans[s.top()] = -1;
+		s.pop();
+	}
+
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << ans[i] << ' ';
+	}
+}
