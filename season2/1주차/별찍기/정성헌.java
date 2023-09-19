5ef2117407f88f28026c92557413774aa4f4d65c
seongheonjeong <js504040@naver.com>
2023-09-20 01:11:54 +0900

    Feat:별 찍기 7번 문제 풀이 추가

--- season2/1주차/별찍기/정성헌.java
+++ season2/1주차/별찍기/정성헌.java
@@ -1,5 +1,4 @@
-//6번
-
+//7번
 
 import java.util.Scanner;
 
@@ -8,10 +7,29 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+
+		
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
+		
 		for(int i=0; i<N; i++)
+		{
+			
+			for(int x=i; x<N-1; x++)
+			{
+				System.out.print(' ');
+			}
+			for(int j=0; j<i*2+1; j++)
+			{
+				System.out.print("*");
+				
+			}
+												
+			System.out.println();
+		}
+		
+		
+		for(int i=1; i<N; i++)
 		{
 			
 			for(int x=0; x<i; x++)
@@ -22,13 +40,46 @@ public class Main
 			{
 				System.out.print("*");
 				
-			}
-											
+			}											
 			System.out.println();
-		}
+		}	
+		sc.close();
 	}
 
 }
+
+
+//6번
+
+
+// import java.util.Scanner;
+
+// public class Main
+// {
+
+// 	public static void main(String[] args)
+// 	{
+// 		// TODO Auto-generated method stub
+// 		Scanner sc=new Scanner(System.in);
+// 		int N=sc.nextInt();
+// 		for(int i=0; i<N; i++)
+// 		{
+			
+// 			for(int x=0; x<i; x++)
+// 			{
+// 				System.out.print(' ');
+// 			}
+// 			for(int j=0; j<N*2-1-(i*2); j++)
+// 			{
+// 				System.out.print("*");
+				
+// 			}
+											
+// 			System.out.println();
+// 		}
+// 	}
+
+// }
 
 // 5번
 
