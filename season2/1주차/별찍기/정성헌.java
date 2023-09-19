7e953a1e54b149b14ff0090f960a7d404b426698
seongheonjeong <js504040@naver.com>
2023-09-20 01:43:29 +0900

    Feat:1주차 정성헌 별 찍기 8,9번 코드 추가

--- season2/1주차/별찍기/정성헌.java
+++ season2/1주차/별찍기/정성헌.java
@@ -1,5 +1,4 @@
-//7번
-
+//9번
 import java.util.Scanner;
 
 public class Main
@@ -12,7 +11,23 @@ public class Main
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
 		
+		
 		for(int i=0; i<N; i++)
+		{
+			
+			for(int x=0; x<i; x++)
+			{
+				System.out.print(' ');
+			}
+			for(int j=0; j<N*2-1-(i*2); j++)
+			{
+				System.out.print("*");
+				
+			}											
+			System.out.println();
+		}	
+
+		for(int i=1; i<N; i++)
 		{
 			
 			for(int x=i; x<N-1; x++)
@@ -27,26 +42,112 @@ public class Main
 												
 			System.out.println();
 		}
-		
-		
-		for(int i=1; i<N; i++)
-		{
-			
-			for(int x=0; x<i; x++)
-			{
-				System.out.print(' ');
-			}
-			for(int j=0; j<N*2-1-(i*2); j++)
-			{
-				System.out.print("*");
-				
-			}											
-			System.out.println();
-		}	
 		sc.close();
 	}
 
 }
+//8번
+// import java.util.Scanner;
+
+// public class Main
+// {
+
+// 	public static void main(String[] args)
+// 	{
+
+		
+// 		Scanner sc=new Scanner(System.in);
+// 		int N=sc.nextInt();
+		
+		
+// 		for(int i=1; i<=N; i++)
+// 		{						
+// 			for(int j=0; j<i; j++)
+// 			{
+// 				System.out.print("*");
+// 			}
+// 			for(int x=0; x<N*2-(i*2); x++)
+// 			{
+// 				System.out.print(' ');
+// 			}	
+// 			for(int y=0; y<i; y++)
+// 			{
+// 				System.out.print("*");
+// 			}										
+// 			System.out.println();
+// 		}			
+
+// 		for(int i=2; i<=N; i++)
+// 		{						
+// 			for(int j=i-1; j<N; j++)
+// 			{
+// 				System.out.print("*");
+// 			}
+// 			for(int x=0; x<(i-1)*2; x++)
+// 			{
+// 				System.out.print(' ');
+// 			}	
+// 			for(int y=i-1; y<N; y++)
+// 			{
+// 				System.out.print("*");
+// 			}									
+// 			System.out.println();
+// 		}			
+// 		sc.close();
+// 	}
+
+// }
+
+
+//7번
+
+// import java.util.Scanner;
+
+// public class Main
+// {
+
+// 	public static void main(String[] args)
+// 	{
+
+		
+// 		Scanner sc=new Scanner(System.in);
+// 		int N=sc.nextInt();
+		
+// 		for(int i=0; i<N; i++)
+// 		{
+			
+// 			for(int x=i; x<N-1; x++)
+// 			{
+// 				System.out.print(' ');
+// 			}
+// 			for(int j=0; j<i*2+1; j++)
+// 			{
+// 				System.out.print("*");
+				
+// 			}
+												
+// 			System.out.println();
+// 		}
+		
+		
+// 		for(int i=1; i<N; i++)
+// 		{
+			
+// 			for(int x=0; x<i; x++)
+// 			{
+// 				System.out.print(' ');
+// 			}
+// 			for(int j=0; j<N*2-1-(i*2); j++)
+// 			{
+// 				System.out.print("*");
+				
+// 			}											
+// 			System.out.println();
+// 		}	
+// 		sc.close();
+// 	}
+
+// }
 
 
 //6번
