e35c2f03f6f4583aae66f85b28dcef9d46745db4
GlintJade <144599260+GlintJade@users.noreply.github.com>
2024-01-01 23:36:11 +0900

    Feat : 1주차 오민경 행렬덧셈 문제풀이 - 배열 1개 최최종

--- season3/1주차/행렬덧셈/오민경.cpp
+++ season3/1주차/행렬덧셈/오민경.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main() {
     int size1, size2;
-    cin >> size1 >> size2; 
-    int** array1 = new int* [size1*2];
-    for (int i = 0; i < size1; i++) {
+    cin >> size1 >> size2;
+    int** array1 = new int* [size1 * 2];
+    for (int i = 0; i < size1*2; i++) {
         array1[i] = new int[size2];
     }
-    for (int i = 0; i < size1*2; i++) {
+    for (int i = 0; i < size1 * 2; i++) {
         for (int j = 0; j < size2; j++) {
             cin >> array1[i][j];
         }
     }
     for (int i = 0; i < size1; i++) {
         for (int j = 0; j < size2; j++) {
-            array1[i][j] += array[i*2][j];
-            cout<<array1[i][j]<<" ";
+            array1[i][j] += array1[i + size1][j];
+            cout << array1[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
