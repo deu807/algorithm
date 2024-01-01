25ae2976e7d9043efd87db32a6d12e9fe34351a6
GlintJade <144599260+GlintJade@users.noreply.github.com>
2024-01-01 23:29:02 +0900

    Feat : 1주차 오민경 행렬덧셈 문제풀이 - 배열 1개

--- season3/1주차/행렬덧셈/오민경.cpp
+++ season3/1주차/행렬덧셈/오민경.cpp
@@ -4,37 +4,21 @@ using namespace std;
 int main() {
     int size1, size2;
     cin >> size1 >> size2; 
-    int** array1 = new int* [size1]; 
-    int** array2 = new int* [size1];
-    int** answer = new int* [size1];
+    int** array1 = new int* [size1*2];
     for (int i = 0; i < size1; i++) {
         array1[i] = new int[size2];
-        array2[i] = new int[size2];
-        answer[i] = new int[size2];
     }
-    for (int i = 0; i < size1; i++) {
+    for (int i = 0; i < size1*2; i++) {
         for (int j = 0; j < size2; j++) {
             cin >> array1[i][j];
         }
     }
     for (int i = 0; i < size1; i++) {
         for (int j = 0; j < size2; j++) {
-            cin >> array2[i][j];
+            array1[i][j] += array[i*2][j];
+            cout<<array1[i][j]<<" ";
         }
+        cout<<endl;
     }
-    for (int i = 0; i < size1; i++) {
-        for (int j = 0; j < size2; j++) {
-            answer[i][j] = array1[i][j] + array2[i][j];
-            
-        }
-    }
-    for (int i = 0; i < size1; i++) {
-        for (int j = 0; j < size2; j++) {
-            cout << answer[i][j]<<" ";
-        }
-        cout << endl;
-    }
-    
-
     return 0;
 }
