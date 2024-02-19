12b5adb64907edf7cec097ab40bac0f1f1c6c303
GlintJade <144599260+GlintJade@users.noreply.github.com>
2024-02-19 15:43:15 +0900

    Feat : 2주차 오민경 수들의 합 문제풀이 수정

--- season3/2주차/수들의 합/오민경.cpp
+++ season3/2주차/수들의 합/오민경.cpp
@@ -4,12 +4,11 @@ int main() {
 	long long N, sum = 0;
 	int i = 1, count = 0;
 	cin >> N;
-	while (1) {
+    
+	while (sum <= N) {
 		sum += i;
-		count++;
-		if (sum > N) { count--; break; }
 		i++;
 	}
-	cout << count << "\n";
+	cout << i-1;
 	return 0;
 }
